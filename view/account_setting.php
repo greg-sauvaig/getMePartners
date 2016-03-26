bf4f6d60f247320055a01da618e20fc62225654e
gregoire sauvaigo <greg.sauvaigo@gmail.com>
2016-03-26 16:32:14 +0100

    fix upload
    fix upload for user avatar

--- view/account_setting.php
+++ view/account_setting.php
@@ -50,7 +50,7 @@
 	}
 	
 ?>	<fieldset >changez votre photo de profil ici:		
-	<form method="post" enctype="multipart/form-data" action="./index.php">
+	<form method="post" enctype="multipart/form-data" action="./model/upload.php">
 		<p>
 			<input type="file" name="fichier" size="30">
 			<input type="submit" name="upload" value="Uploader">
