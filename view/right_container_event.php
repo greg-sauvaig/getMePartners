336c134ce928b3474f851865389442d14de5b830
ZMomo <mh.zeghdoudi@gmail.com>
2016-03-26 18:24:50 +0100

    delete fichiers obsoletes

--- view/right_container_event.php
+++
@@ -1,9 +0,0 @@
-<div class="col-lg-1 col-md-1 col-xs-1 col-sm-1"></div>
-<div class="col-lg-9 col-md-9 col-xs-9 col-sm-9" style="border:1px solid black;max-height:60vh;overflow:auto;">
-    <div class="row" style="margin-top:1vh; margin-bottom:1vh;">
-        <div class="col-lg-2 col-md-2 col-xs-2 col-sm-2">
-
-        </div>
-        <?php require_once 'view/event_box.php'; ?>
-    </div>  
-</div>
